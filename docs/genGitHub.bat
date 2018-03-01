d343e116ae07993aa9d743b7e3dbbc671a6b09fb
Chris Korda <ckorda@gmail.com>
2018-03-01 18:54:41 -0500

    upload website

---
+++ docs/genGitHub.bat
@@ -0,0 +1,30 @@
+@echo off
+C:\Chris\tools\navgen templateGitHub.html .
+if errorlevel 1 goto err
+"C:\Chris\MyProjects\FixSelfUrl\Release\FixSelfUrl" *.html
+if errorlevel 1 goto err
+md Help
+C:\Chris\MyProjects\doc2web\release\doc2web /nospaces C:\Chris\MyProjects\WaveShop\Help\help.txt Help Contents.htm C:\Chris\MyProjects\WaveShop\doc\WaveShopHelp.htm "WaveShop Help"
+if errorlevel 1 goto err
+cd Help
+md images
+copy C:\Chris\MyProjects\WaveShop\Help\images\*.* images
+copy ..\helptopic.css content.css
+C:\Chris\tools\navgen C:\Chris\MyProjects\WaveShop\Help\template.txt .
+copy ..\helpheader.txt x
+copy x + Contents.htm
+echo ^<body^>^<html^> >>x
+del Contents.htm
+ren x Contents.htm
+md printable
+cd printable
+move C:\Chris\MyProjects\WaveShop\doc\WaveShopHelp.htm .
+cd ..
+cd ..
+ren issues.html issues.htm
+echo y | C:\Chris\tools\fsr issues.htm "<div id=body>" "<div id=widebody>"
+ren issues.htm issues.html
+goto exit
+:err
+pause Error!
+:exit
